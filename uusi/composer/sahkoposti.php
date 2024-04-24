c19e65cd2f49cb77a236e88bba39b3fd270eb3a8
JoniPiippo <joni.piippo@meili.fi>
2024-04-24 20:27:39 +0300

    24.4
    project icon not working, footer links incorrect, unalignment issues.

--- uusi/composer/sahkoposti.php
+++ uusi/composer/sahkoposti.php
@@ -14,9 +14,9 @@ $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 $mail->Port = 465;
 
 if (!empty($_POST)) {
-    $asiakas = $_POST['email'];
-    $otsikko = $_POST['otsikko'];
-    $viesti = $_POST['text'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['text'];
 
     if (empty($email)) {
         $errors[] = 'Email is empty';
@@ -24,21 +24,17 @@ if (!empty($_POST)) {
         $errors[] = 'Email is invalid';
     }
 
-    $mail->setFrom($asiakas, 'Yhteydenotto');
+    $mail->setFrom($email, 'Yhteydenotto');
     $mail->addAddress('joni.piippo@meili.fi', 'Minä');
-    $mail->Subject = $otsikko;
-    
-    // Set HTML 
+    $mail->Subject = $subject;
     $mail->isHTML(TRUE);
-    $mail->Body = $viesti;
-    $mail->AltBody = 'Hi there, we are happy to confirm your booking. Please check the document in the attachment.';
+    $mail->Body = $message;
 
     // send the message
     if (!$mail->send()) {
         echo 'email ei lähtenyt.';
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     } else {
-        echo 'email on lähtenyt';
         header('location: ../yhteys.html');
     }
 }
